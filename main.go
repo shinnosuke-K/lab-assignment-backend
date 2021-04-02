0a38d547b063629a95525327bbb44c6d2acb3bbd
shinnosuke <shin.050195@gmail.com>
2021-04-02 23:41:38 +0900

    add: パスワードによる処理の変更

--- main.go
+++ main.go
@@ -7,31 +7,66 @@ import (
 	"net/http"
 )
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Login")
+	var form struct {
+		UserId   string `json:"user_id"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "ng")
+		return
+	}
+
+	res := struct {
+		User struct {
+			UserID  string `json:"user_id"`
+			Entered bool   `json:"entered"`
+		} `json:"user"`
+	}{}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	switch form.Password {
+	case "a":
+		res.User.UserID = "1316190124"
+		res.User.Entered = false
+		if err := json.NewEncoder(w).Encode(&res); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		log.Println(res)
+		return
+
+	case "b":
+		res.User.UserID = "1316190123"
+		res.User.Entered = true
+		if err := json.NewEncoder(w).Encode(&res); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		log.Println(res)
+		return
+
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "ng")
+	}
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Logout")
+	log.Println(w.Header())
+}
+
 func main() {
-	http.HandleFunc("/auth/login", func(w http.ResponseWriter, r *http.Request) {
-
-		var form struct {
-			UserId   string `json:"user_id"`
-			Password string `json:"password"`
-		}
-
-		if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "ng")
-			return
-		}
-
-		log.Println(form)
-
-		if form.Password != "a" {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "ng")
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "ok")
-	})
+	http.HandleFunc("/auth/login", LoginHandler)
+	http.HandleFunc("/auth/logout", LogoutHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
