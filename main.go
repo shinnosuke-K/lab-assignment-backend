ec5844916c88d8d70412de3a329e0f41ecb8aab7
shinnosuke-K <shin.050195@gmail.com>
2021-04-11 20:25:00 +0900

    add: 入力結果を保存するハンドラの花形を作成

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -65,6 +66,46 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Logout")
 	log.Println(w.Header())
+}
+
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Save")
+
+	body, _ := io.ReadAll(r.Body)
+	type Request struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+	}
+
+	log.Println(string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var req Request
+	if err := json.Unmarshal(body, &req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	//if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	log.Println(err)
+	//	return
+	//}
+
+	log.Println(req)
+
+	type Response struct {
+		Msg string `json:"msg"`
+	}
+
+	res := Response{Msg: "thank you"}
+
+	b, _ := json.Marshal(res)
+	fmt.Fprintf(w, string(b))
+	return
 }
 
 func main() {
