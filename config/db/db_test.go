48ecd2c152f79efa86640e9a05a8847131237bf1
shinnosuke-K <shin.050195@gmail.com>
2021-05-29 20:44:43 +0900

    add: データ挿入目的とテスト実行のために作成

---
+++ config/db/db_test.go
@@ -0,0 +1,76 @@
+package db
+
+import "testing"
+
+func TestInsertLabs(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:    "教授情報挿入",
+			args:    args{path: "../../lab.csv"},
+			wantErr: false,
+		},
+		{
+			name:    "違うファイルパス",
+			args:    args{path: "lab.csv"},
+			wantErr: true,
+		},
+		{
+			name:    "csvファイルではない",
+			args:    args{path: "../../main.go"},
+			wantErr: true,
+		},
+	}
+
+	Open()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := InsertLabs(tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("InsertLabs() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestInsertUsers(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:    "学生情報挿入",
+			args:    args{path: "../../student.csv"},
+			wantErr: false,
+		},
+		{
+			name:    "違うファイルパス",
+			args:    args{path: "student.csv"},
+			wantErr: true,
+		},
+		{
+			name:    "csvファイルではない",
+			args:    args{path: "../../main.go"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := InsertUsers(tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("InsertUsers() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
