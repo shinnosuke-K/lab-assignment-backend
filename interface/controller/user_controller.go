5e044fcfe820da31371a1ad58f989a80fb082844
shinnosuke-K <shin.050195@gmail.com>
2021-05-30 14:25:16 +0900

    add: controller層の雛形

---
+++ interface/controller/user_controller.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/shinnosuke-K/lab-assignment-backend/service"
+)
+
+type UserViewController struct {
+	Service service.UserViewService
+}
+
+func NewUserViewController(s *service.UserViewServiceImpl) *UserViewController {
+	return &UserViewController{
+		Service: s,
+	}
+}
+
+func (u *UserViewController) Handler(w http.ResponseWriter, r *http.Request) {
+}
