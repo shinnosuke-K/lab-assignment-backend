8006261849bb3df6fe59348048a6d79d169cbb2c
shinnosuke-K <shin.050195@gmail.com>
2021-06-01 13:32:25 +0900

    add: store層の雛形を用意

--- infrastructure/datastore/professor_store.go
+++ infrastructure/datastore/professor_store.go
@@ -1,1 +1,16 @@
 package datastore
+
+import (
+	"github.com/shinnosuke-K/lab-assignment-backend/domain/model"
+	"github.com/shinnosuke-K/lab-assignment-backend/domain/repository"
+)
+
+type ProfStore struct{}
+
+func (*ProfStore) GetAll(db repository.DBHandler) ([]model.Professor, error) {
+	return nil, nil
+}
+
+func (*ProfStore) GetByID(db repository.DBHandler, id string) (*model.Professor, error) {
+	return &model.Professor{}, nil
+}
