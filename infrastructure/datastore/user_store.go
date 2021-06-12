5108f3e950712b5b21597f6aef2fb4c3acf7b095
shinnosuke-K <shin.050195@gmail.com>
2021-06-12 15:38:38 +0900

    add: データ取得処理

--- infrastructure/datastore/user_store.go
+++ infrastructure/datastore/user_store.go
@@ -2,6 +2,7 @@ package datastore
 
 import (
 	"github.com/pkg/errors"
+	"github.com/shinnosuke-K/lab-assignment-backend/config/db"
 	"github.com/shinnosuke-K/lab-assignment-backend/domain/model"
 	"github.com/shinnosuke-K/lab-assignment-backend/domain/repository"
 )
@@ -14,18 +15,54 @@ func (*UserStore) GetByID(db repository.DBHandler, studentNum int) (*model.User,
 			select * from users where student_num = ?
 		`
 
-	user := new(model.User)
+	var user model.User
 	if err := db.QueryRowx(q, studentNum).StructScan(&user); err != nil {
-		return &model.User{}, errors.Wrap(err, "UserStore.GetByID go error")
+		return nil, errors.Wrap(err, "UserStore.GetByID go error")
 	}
 
-	return user, nil
+	return &user, nil
 }
 
-func (*UserStore) UpdateGraduate(db repository.DBHandler, graduate bool) error {
+func (*UserStore) UpdateGraduate(db repository.DBHandler, studentNum int, graduate bool) error {
+
+	if !exitsRecord(studentNum) {
+		return errors.Errorf("no record with student_num = %d \n", studentNum)
+	}
+
+	q := `
+		update users set graduate = ? where student_num = ?
+		`
+
+	if _, err := db.Exec(q, graduate, studentNum); err != nil {
+		return errors.Wrap(err, "UserStore.UpdateGraduate got error")
+	}
 	return nil
 }
 
-func (*UserStore) UpdateEntered(db repository.DBHandler) error {
+func (*UserStore) UpdateEntered(db repository.DBHandler, studentNum int) error {
+
+	if !exitsRecord(studentNum) {
+		return errors.Errorf("no record with student_num = %d \n", studentNum)
+	}
+
+	q := `
+		update users set entered = ? where student_num = ?
+		`
+
+	if _, err := db.Exec(q, true, studentNum); err != nil {
+		return errors.Wrap(err, "UserStore.UpdateEntered got error")
+	}
+
 	return nil
+}
+
+func exitsRecord(studentNum int) bool {
+	q := `
+			select * from users where student_num = ? limit 1
+		`
+	var user model.User
+	if err := db.Driver.QueryRowx(q, studentNum).StructScan(&user); err != nil {
+		return false
+	}
+	return true
 }
